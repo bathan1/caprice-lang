6a906a30dbc3d6b47bbedfee4425464345eeb049
Brandon Stride <brandonzstride@hotmail.com>
2026-02-09 10:17:10 -0500

    document test presets and add diverge preset

--- src-test/preset.ml
+++ src-test/preset.ml
@@ -7,7 +7,18 @@ type t = Preset of Ident.t [@@unboxed]
 let s (Ident.Ident id) = id
 
 (*
-  Preset: splaying can quickly prove this well typed
+  Preset: splaying can quickly prove this well typed.
+
+  (* TEST
+    typing = exhausted;
+    speed = fast;
+    flags += " -s -r";
+    typecheck; 
+  *)
+    ===
+  (* TEST 
+    include splayable;
+  *)
 *)
 let splayable : Ctl_ast.t =
   [ Env_stmt (Assign (speed, s fast))
@@ -17,7 +28,18 @@ let splayable : Ctl_ast.t =
   ]
 
 (*
-  Preset: there is a refutation, i.e. a path that shows the program is ill-typed
+  Preset: there is a refutation, i.e. a path that shows the program is ill-typed.
+
+  (* TEST
+    typing = ill-typed;
+    speed = fast;
+    flags += " -r";
+    typecheck; 
+  *)
+    ===
+  (* TEST 
+    include refutable;
+  *)
 *)
 let refutable : Ctl_ast.t =
   [ Env_stmt (Assign (speed, s fast))
@@ -27,7 +49,18 @@ let refutable : Ctl_ast.t =
   ]
 
 (*
-  Preset: there are naturally finitely many paths
+  Preset: there are naturally finitely many paths.
+
+  (* TEST
+    typing = exhausted;
+    speed = fast;
+    flags += " -r";
+    typecheck; 
+  *)
+    ===
+  (* TEST 
+    include finite-well-typed;
+  *)
 *)
 let finite_well_typed : Ctl_ast.t =
   [ Env_stmt (Assign (speed, s fast))
@@ -36,8 +69,31 @@ let finite_well_typed : Ctl_ast.t =
   ; Test Typecheck
   ]
 
+(*
+  Preset: there are infinitely many paths, and splaying would be
+    incomplete; we are expecting timeout.
+
+  (* TEST
+    typing = no-error;
+    speed = slow;
+    flags += " -t 3.0 -r";
+    typecheck; 
+  *)
+    ===
+  (* TEST 
+    include diverges;
+  *)
+*)
+let diverges : Ctl_ast.t =
+  [ Env_stmt (Assign (speed, s slow))
+  ; Env_stmt (Assign (typing, s no_error))
+  ; Env_stmt (Append (flags, " -t 3.0 -r"))
+  ; Test Typecheck
+  ]
+
 let lookup : ident -> Ctl_ast.t = function
   | Ident "finite-well-typed" -> finite_well_typed
   | Ident "splayable" -> splayable
   | Ident "refutable" -> refutable
+  | Ident "diverges" -> diverges
   | _ -> []
