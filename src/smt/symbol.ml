33c1c6b4d079967953d185bc81499a4c62c69092
Brandon Stride <brandonzstride@hotmail.com>
2026-02-26 12:57:21 -0500

    simpler smt symbols

--- src/smt/symbol.ml
+++ src/smt/symbol.ml
@@ -10,28 +10,35 @@ module Make_comparable_key (K : KEY) = struct
   let equal a b = Utils.Uid.equal (uid a) (uid b)
 end
 
-module X = Utils.Separate.Comparable.Make (Utils.Uid)
+(* Symbols have a phantom 'k (key) parameter. The underlying
+  key is actually a Uid.t *)
+type ('a, 'k) t = ('a, Utils.Uid.t) s
+and (_, 'b) s =
+  | I : 'b -> (int, 'b) s
+  | B : 'b -> (bool, 'b) s
 
-module T = struct
-  type ('a, 'k) t = 'a X.t
+let compare (type a) (x : (a, 'k) t) (y : (a, 'k) t) : int =
+  match x, y with
+  | I xi, I yi
+  | B xi, B yi -> Utils.Uid.compare xi yi
 
-  let equal = X.equal
+let equal (type a) (x : (a, 'k) t) (y : (a, 'k) t) : bool =
+  match x, y with
+  | I xi, I yi
+  | B xi, B yi -> Utils.Uid.equal xi yi
 
-  let make_int (k : 'k) (uid : 'k -> Utils.Uid.t) : (int, 'k) t =
-    I (uid k)
+let make_int (k : 'k) (uid : 'k -> Utils.Uid.t) : (int, 'k) t =
+  I (uid k)
 
-  let make_bool (k : 'k) (uid : 'k -> Utils.Uid.t) : (bool, 'k) t =
-    B (uid k)
-end
-
-include T
+let make_bool (k : 'k) (uid : 'k -> Utils.Uid.t) : (bool, 'k) t =
+  B (uid k)
 
 module Make (Key : KEY) = struct
-  type 'a t = ('a, Key.t) T.t
+  type nonrec 'a t = ('a, Key.t) t
 
   let make_int (k : Key.t) : int t =
-    T.make_int k Key.uid
+    make_int k Key.uid
 
   let make_bool (k : Key.t) : bool t =
-    T.make_bool k Key.uid
+    make_bool k Key.uid
 end
