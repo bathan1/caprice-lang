3a01e2d0290505a246842c7519b2fe2eeca8328c
bathan1 <side_nathan@hotmail.com>
2026-04-09 21:01:18 -0400

    Port over rewrite

--- src/smt/solve.ml
+++ src/smt/solve.ml
@@ -1,3 +1,4 @@
+open Utils
 
 module type SOLVABLE = sig
   include Formula.S
@@ -78,17 +79,225 @@ let rec propagate_constants : 'k simplifier = fun solve expr ->
   | _ ->
     solve expr
 
-(** TODO: Port over `rewrite` + friends here *)
-module Simplify_bounds = struct
-end
+type int_bound = {
+  lower : int option;
+  upper : int option;
+  nots : int list;
+}
+
+let append_neq 
+  (uid : Uid.t)
+  (val_neq : int)
+  (bounds_state : (Uid.t * int_bound) list) =
+  let existing =
+    List.assoc_opt uid bounds_state
+    |> Option.value ~default:{ lower=None; upper=None; nots=[]; }
+  in
+  let within_bounds =
+    let lower_ok =
+      match existing.lower with
+      | None -> true
+      | Some l -> val_neq >= l
+    in
+    let upper_ok =
+      match existing.upper with
+      | None -> true
+      | Some u -> val_neq <= u
+    in
+    lower_ok && upper_ok
+  in
+
+  let appended =
+    if not within_bounds then
+      existing
+    else
+      { existing with
+        nots =
+          if List.mem val_neq existing.nots
+          then existing.nots
+          else val_neq :: existing.nots
+      }
+  in
+
+  (uid, appended)
+  :: List.remove_assoc uid bounds_state
+
+let update_bounds (uid : Uid.t) f bounds_state =
+  let existing =
+    List.assoc_opt uid bounds_state
+    |> Option.value ~default:{ lower=None; upper=None; nots=[] }
+  in
+  let updated = f existing in
+  (uid, updated)
+  :: List.remove_assoc uid bounds_state
+
+let rec rewrite_int : type k.
+  (int, k) Formula.t -> (int, k) Formula.t =
+  function
+  | Formula.Binop (Plus, a, b) ->
+    Formula.binop Plus (rewrite_int a) (rewrite_int b)
+  | Binop (Minus, a, b) ->
+    Formula.binop Minus (rewrite_int a) (rewrite_int b)
+  | t -> t
+let rec linearize : type k.
+  (int, k) Formula.t -> (Uid.t * int) option =
+  function
+  | Formula.Key (I x) ->
+    Some (x, 0)
+  | Binop (Plus, t, Const_int c) ->
+    linearize t
+    |> Option.map (fun (x, k) -> (x, k + c))
+  | Binop (Plus, Const_int c, t) ->
+    linearize t
+    |> Option.map (fun (x, k) -> (x, k + c))
+
+  | Binop (Minus, t, Const_int c) ->
+    linearize t
+    |> Option.map (fun (x, k) -> (x, k - c))
+  | _ ->
+    None
+
+(** TODO: Port over `rewrite` + friends here. *)
+let bounds_rewrite : 'k simplifier = fun solve expr ->
+  let rec loop_over 
+    (bounds_state : (Uid.t * int_bound) list)
+    (rest : (bool, 'k) Formula.t list) =
+    function
+    | Formula.Not (Formula.Binop (Binop.Equal, Const_int c, Key (I x)))
+    | Not (Binop (Equal, Key (I x), Const_int c))
+    | Binop (Not_equal, Const_int c, Key (I x))
+    | Binop (Not_equal, Key (I x), Const_int c) -> (
+      append_neq x c bounds_state, rest
+    )
+    | Binop (Less_than_eq, Const_int c1, rhs) -> 
+      loop_over bounds_state rest (
+        Formula.binop Greater_than_eq rhs (Formula.const_int c1)
+      )
+    | Binop (Less_than, Const_int c1, rhs) ->
+      loop_over bounds_state rest (
+        Formula.binop Greater_than rhs (Formula.const_int c1)
+      )
+    | Binop (Greater_than_eq, Const_int c1, rhs) ->
+      loop_over bounds_state rest (
+        Formula.binop Less_than_eq rhs (Formula.const_int c1)
+      )
+    | Binop (Greater_than, Const_int c1, rhs) ->
+      loop_over bounds_state rest (
+        Formula.binop Less_than rhs (Formula.const_int c1)
+      )
+    | Not (Binop (Less_than, a, b)) ->
+      loop_over bounds_state rest (
+        Formula.binop Greater_than_eq a b
+      )
+    | Not (Binop (Less_than_eq, a, b)) ->
+      loop_over bounds_state rest (
+        Formula.binop Greater_than a b
+      )
+    | Not (Binop (Greater_than, a, b)) ->
+      loop_over bounds_state rest (
+        Formula.binop Less_than_eq a b
+      )
+    | Not (Binop (Greater_than_eq, a, b)) ->
+      loop_over bounds_state rest (
+        Formula.binop Less_than a b
+      )
+    | Binop ((Less_than | Less_than_eq
+      | Greater_than | Greater_than_eq) as op,
+      lhs,
+      Const_int c2) -> (
+      let lhs = rewrite_int lhs in
+        match linearize lhs with
+        | Some (x, k') ->
+          let c = c2 - k' in 
+          let bounds_state = (
+            match op with
+            | Less_than_eq -> (
+                update_bounds 
+                  x 
+                  (fun b -> {
+                    b with upper = Some (
+                      match b.upper with
+                      | None -> c
+                      | Some u -> min u c
+                    )
+                  })
+                  bounds_state
+              )
+            | Less_than -> (
+                update_bounds
+                  x
+                  (fun b -> { 
+                    b with upper = Some (
+                      match b.upper with
+                      | None -> c - 1
+                      | Some u -> min u (c - 1)
+                    ) 
+                  })
+                  bounds_state
+              )
+            | Greater_than_eq -> (
+                update_bounds 
+                  x 
+                  (fun b -> {
+                    b with lower = Some (
+                      match b.lower with
+                      | None -> c
+                      | Some l -> max l c
+                    )
+                  })
+                  bounds_state
+              )
+            | Greater_than -> (
+                update_bounds
+                  x
+                  (fun b -> {
+                    b with lower = Some (
+                      match b.lower with
+                      | None -> c + 1
+                      | Some l -> max l (c + 1)
+                    )
+                  })
+                  bounds_state
+              )
+            | _ -> bounds_state
+          ) in (bounds_state, rest)
+        | None ->
+          (* Should never hit... *)
+          (bounds_state, Formula.binop op lhs (Formula.const_int c2) :: rest)
+      )
+    | And xs ->
+      xs
+      |> List.fold_left (fun (st, rest_acc) f -> 
+        loop_over st rest_acc f
+      ) (bounds_state, rest)
+    | f -> (bounds_state, f :: rest)
+  in
+  solve expr
 
 (** TODO: Port over `bellman_ford` + friends here. *)
-let idl : 'k simplifier = fun solve expr ->
+let integer_difference : 'k simplifier = fun solve expr ->
   solve expr
+
+(*
+  Right-associative simplifier composition.
+  E.g. this simplifier
+    simpl1 @> simpl2 @> simpl3
+  is equivalent to
+    simpl1 @> (simpl2 @> simpl3)
+  which we can see does simpl1 as a pre-pass to the composition of simpl2
+  and simpl3. Hence when given a solver, this first simplifies with simpl1,
+  then with simpl2, and finally with simpl3 before calling the solver.
+*)
+let (@>) : 'k simplifier -> 'k simplifier -> 'k simplifier =
+  fun f g ->
+    fun solve ->
+      g (f solve)
 
 (** TODO: Replace direct_solve with concolic/loop.ml *)
 let main_solve (module Oracle : SOLVABLE) : 'k solver =
-  propagate_constants
-  @@ idl
-  @@ direct_solve (module Oracle)
+  let pipeline = bounds_rewrite
+  @> propagate_constants
+  @> integer_difference 
+  in
+  pipeline (direct_solve (module Oracle))
 
